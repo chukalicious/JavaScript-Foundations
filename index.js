93c6a40a82dd9acfdc49691fadf5d428fc02f6b1
Katiuska <katiuskaalicea@gmail.com>
2020-05-26 20:19:48 -0700

    completed task 3

--- index.js
+++ index.js
@@ -4,12 +4,10 @@
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
 
-const principal = 200000;
-let interest = .05;
+let principal = 200000;
+let interestRate = .05;
 const years = 30; 
-
-
-
+const name = "Kat"; 
 
 
 
@@ -21,8 +19,8 @@ Create a variable called `monthlyInterestRate` and give it the value of interest
 
 Create another variable called `periods` and give it the value of years*12.
 */
- const monthlyInterestRate = interest/12;
- const periods = years*12;
+ const monthlyInterestRate = interestRate / 12;
+ const periods = years * 12;
 
 
 
@@ -36,8 +34,15 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 When your math is correct, monthlyRate will equal 1073.64
 */
 
+//M = P [ I ( 1 + I )^N ] / [ ( 1 + I )^N – 1 ]
 
 
+let innerNumerator = 1 + monthlyInterestRate; 
+let numerator = monthlyInterestRate * Math.pow(innerNumerator, periods).toFixed(6);
+let innerDenominator = 1 + monthlyInterestRate; 
+let denominator = Math.pow(innerDenominator, periods) - 1; 
+
+ 
 
 // 🏡 Task 3: Function
 /* Create a function called `mortgageCalculator` that combines all of the steps from task 1 and 2 and returns a sentence "{Name}, your monthly rate is ${monthlyRate}"
@@ -45,6 +50,14 @@ When your math is correct, monthlyRate will equal 1073.64
 If your name is `Oscar` mortgageCalculator() should return "Oscar, your monthly rate is 1073.64"
 */
 
+function mortgageCalculator(name) {
+    let division = numerator/denominator; 
+    let monthlyRate = principal * division; //.toFixed(2)
+    return `${name}, your monthly rate is ${monthlyRate.toFixed(2)}`; 
+
+}
+
+mortgageCalculator('Kat')
 
 
 
@@ -56,7 +69,8 @@ For example,
 mortgageCalculator(200000, 0.05, 30); <-- should return 1,073.64
 */
 
-
+let P = principal; 
+let I = interestRate; 
 
 
 
